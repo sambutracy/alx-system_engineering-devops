657d56e699a6e60cea90cd48bfeee6b50c5dac22
sambutracy <tracysambut@gmail.com>
2024-04-24 17:20:37 -0700

    update advanced tasks

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,28 @@
-#!/usr/bin/env bash
-# This script sets up a new Ubuntu machine by installing Nginx.
-# Nginx will be configured to listen on port 80 and serve a basic "Hello World" page.
-#
-echo -e "Updating and installing Nginx.\n"
-sudo apt-get update -y -qq && \
-	 sudo apt-get install nginx -y
+# Update the Ubuntu system and install Nginx.
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
-echo -e "\nSetting up some minor stuff.\n"
+# Ensure Nginx package is installed.
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
 
-# Start Nginx service
-sudo service nginx start
+# Create an HTML index file with "Hello World!" content.
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
 
-# Allow Nginx through firewall
-sudo ufw allow 'Nginx HTTP'
+# Set up a redirection rule in the Nginx configuration.
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
 
-# Grant user ownership of website files for easier editing
-sudo chown -R "$USER":"$USER" /var/www/html
-sudo chmod -R 755 /var/www
-
-# Backup default index
-cp /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html.bckp
-
-# Create new index
-echo -e "Hello World!" > /var/www/html/index.nginx-debian.html
-
-# Set up redirection to a YouTube video for /redirect_me
-sudo sed -i '24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
-
-# Set up a custom 404 page
-echo "Ceci n'est pas une page" >> /var/www/html/error_404.html
-sudo sed -i '25i\	error_page 404 /error_404.html;' /etc/nginx/sites-available/default
-
-# Restart Nginx
-sudo service nginx restart
-
-echo -e "\nCompleted.\n"
+# Ensure Nginx service is running.
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
 
