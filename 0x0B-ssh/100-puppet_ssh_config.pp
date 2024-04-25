39f67d7abb41dc62044cfc25610b2e3b3b2c4e34
sambutracy <tracysambut@gmail.com>
2024-04-24 17:42:27 -0700

    pupet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,20 @@
+# Use Puppet to configure SSH for passwordless login and disable password authentication.
+
+# Update SSH config to include the specified private key file.
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+# Update SSH config to disable password authentication.
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+
