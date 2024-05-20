6437c696797c22797649b26d1af15785b3aad601
sambutracy <tracysambut@gmail.com>
2024-05-20 15:13:12 -0700

    fix pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,7 @@ Gather data from REST API and display employee TODO loist progress
 import requests
 import sys
 
+
 def fetch_employee_data(employee_id):
     """
     fetch employee data including name and todo list progress.
@@ -15,7 +16,7 @@ def fetch_employee_data(employee_id):
     user_url = f"{base_url}users/{employee_id}"
     todos_url = f"{base_url}todos?userId={employee_id}"
 
-    #getemployee information
+    # getemployee information
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
         return None, None
@@ -32,6 +33,7 @@ def fetch_employee_data(employee_id):
 
     return employee_name, todos_data
 
+
 def display_todo_progress(employee_name, todos_data):
     """
     Display the TODO list progress of the employee.
@@ -44,10 +46,12 @@ def display_todo_progress(employee_name, todos_data):
     done_tasks = [task for task in todos_data if task.get('completed')]
     number_of_done_tasks = len(done_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks("
+          f"{number_of_done_tasks}/{total_tasks}):")
 
     for task in done_tasks:
         print(f"\t {task.get('title')}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
